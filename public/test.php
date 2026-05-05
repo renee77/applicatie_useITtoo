c3f675d16fdd65741628d4dd19f2386492bd5ad9
Eva <63844661+renee77@users.noreply.github.com>
2026-05-05 16:47:58 +0200

    Fix: test.php en verwijzing database en  bootstrap

--- public/test.php
+++ public/test.php
@@ -1,11 +1,14 @@
 <?php
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../app/Core/Database.php';
+require_once __DIR__ . '/../bootstrap.php'; // ← laadt autoloader + Dotenv
+
+use App\Core\Database; // ← namespace correct met hoofdletters
 
 try {
-    $pdo = Database::getConnection();
+    $pdo = app\Core\Database::getConnection();
     echo 'Verbinding geslaagd!';
 } catch (Exception $e) {
     echo 'Fout: ' . $e->getMessage();
