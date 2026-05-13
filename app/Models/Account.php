d5dc0943c3868fbfe61b756459ef9c51609b2265
Lonneke <lonnekevanoers@gmail.com>
2026-05-13 10:17:49 +0200

    feat(Account): created the construct function

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,38 +2,59 @@
 
 namespace App\Models;
 
+use App\Models\AccountType;
+use DateTime;
+
 class Account
 {
+    private ?int $account_id;
+    private string $email;
+    private string $gebruikersnaam;
+    private string $wachtwoord;
+    private ?string $voornaam;
+    private ?string $achternaam;
+    private DateTime $created_at;
+    private DateTime $geboortedatum;
+    private ?string $telefoon;
+    private AccountType $type;
+    private ?DateTime $deleted_at;
+
     public function __construct(
-        private int $id,
-        private string $name,
-        private string $email,
-        private string $username,
-        private string $password
+        string $email,
+        string $gebruikersnaam,
+        string $wachtwoord,
+        ?string $voornaam = null,
+        ?string $achternaam = null,
+        DateTime $created_at,
+        DateTime $geboortedatum,
+        ?string $telefoon = null,
+        AccountType $type,
+        ?DateTime $deleted_at = null,
+        // Het account_id wordt automatisch toegekend bij een nieuw product via database. 
+        ?int $account_id = null
     ) {
-    }
+        if (strlen($gebruikersnaam) <= 2) {
+            throw new \InvalidArgumentException("Gebruikersnaam moet langer als 2 karakters zijn.");
+        }
 
-    public function getId(): int
-    {
-        return $this->id;
-    }
-    public function getName(): string
-    {
-        return $this->name;
-    }
+        $minDatum = new DateTime();
+        $minDatum->modify('-18 years');
+        if ($geboortedatum > $minDatum) {
+            throw new \InvalidArgumentException(
+                "Geboortedatum moet minimaal 18 jaar geleden zijn."
+            );
+        }
 
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
+        $this->account_id = $account_id;
+        $this->email = $email;
+        $this->gebruikersnaam = $gebruikersnaam;
+        $this->wachtwoord = $wachtwoord;
+        $this->voornaam = $voornaam;
+        $this->achternaam = $achternaam;
+        $this->created_at = $created_at;
+        $this->geboortedatum = $geboortedatum;
+        $this->telefoon = $telefoon;
+        $this->type = $type;
+        $this->deleted_at = $deleted_at;
+    } 
 }
