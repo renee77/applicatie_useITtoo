3a12c282639da0c045f6f29f8d3ab8166c60ef62
Eva <63844661+renee77@users.noreply.github.com>
2026-05-19 15:09:56 +0200

    Feature: klantaccount +tests

--- tests/Unit/BeheerDAOTest.php
+++ tests/Unit/BeheerDAOTest.php
@@ -10,63 +10,63 @@ use PHPUnit\Framework\TestCase;
 
 class BeheerDAOTest extends TestCase
 {
-  public function testGetBeheerByIdReturnsBeheer(): void
-  {
-    // nep PDO-statement en wat er terug moet worden gegeven
-    $stmt = $this->createMock(PDOStatement::class);
-    $stmt->method('fetch')->willReturn([
-      'rol' => 'klantenservice',
-      'datum_in_dienst' => '2023-01-10',
-      'email' => 'jeroendijkstra@example.com',
-      'gebruikersnaam' => 'JDijkstra',
-      'wachtwoord_hash' => '$2y$12$qEiiSq6304LVLoa0wp3hw.jMe575sZ9QrePfFOObrpgV0DjnGgG9q',
-      'created_at' => '2026-05-13',
-      'geboortedatum' => '1980-01-30',
-      'type' => 'beheer',
-      'voornaam' => 'Jeroen',
-      'achternaam' => 'Dijkstra',
-      'telefoon' => '+31610000015',
-      'deleted_at' => null,
-      'account_id' => 15,
-    ]);
+    public function testGetBeheerByIdReturnsBeheer(): void
+    {
+        // nep PDO-statement en wat er terug moet worden gegeven
+        $stmt = $this->createMock(PDOStatement::class);
+        $stmt->method('fetch')->willReturn([
+          'rol' => 'klantenservice',
+          'datum_in_dienst' => '2023-01-10',
+          'email' => 'jeroendijkstra@example.com',
+          'gebruikersnaam' => 'JDijkstra',
+          'wachtwoord_hash' => '$2y$12$qEiiSq6304LVLoa0wp3hw.jMe575sZ9QrePfFOObrpgV0DjnGgG9q',
+          'created_at' => '2026-05-13',
+          'geboortedatum' => '1980-01-30',
+          // 'type' => 'beheer',
+          'voornaam' => 'Jeroen',
+          'achternaam' => 'Dijkstra',
+          'telefoon' => '+31610000015',
+          'deleted_at' => null,
+          'account_id' => 15,
+        ]);
 
-    $mockPdo = $this->createMock(PDO::class);
-    $mockPdo->method('prepare')->willReturn($stmt);
+        $mockPdo = $this->createMock(PDO::class);
+        $mockPdo->method('prepare')->willReturn($stmt);
 
-    $beheerdao = new BeheerDAO($mockPdo);
+        $beheerdao = new BeheerDAO($mockPdo);
 
-    $beheer = $beheerdao->getById(15);
+        $beheer = $beheerdao->getById(15);
 
-    $this->assertInstanceOf(Beheer::class, $beheer);
-  }
+        $this->assertInstanceOf(Beheer::class, $beheer);
+    }
 
-  public function testGetBeheerByUsernameReturnsBeheer(): void
-  {
-    // nep PDO-statement en wat er terug moet worden gegeven
-    $stmt = $this->createMock(PDOStatement::class);
-    $stmt->method('fetch')->willReturn([
-      'rol' => 'klantenservice',
-      'datum_in_dienst' => '2023-01-10',
-      'email' => 'jeroendijkstra@example.com',
-      'gebruikersnaam' => 'JDijkstra',
-      'wachtwoord_hash' => '$2y$12$qEiiSq6304LVLoa0wp3hw.jMe575sZ9QrePfFOObrpgV0DjnGgG9q',
-      'created_at' => '2026-05-13',
-      'geboortedatum' => '1980-01-30',
-      'type' => 'beheer',
-      'voornaam' => 'Jeroen',
-      'achternaam' => 'Dijkstra',
-      'telefoon' => '+31610000015',
-      'deleted_at' => null,
-      'account_id' => 15,
-    ]);
+    public function testGetBeheerByUsernameReturnsBeheer(): void
+    {
+        // nep PDO-statement en wat er terug moet worden gegeven
+        $stmt = $this->createMock(PDOStatement::class);
+        $stmt->method('fetch')->willReturn([
+          'rol' => 'klantenservice',
+          'datum_in_dienst' => '2023-01-10',
+          'email' => 'jeroendijkstra@example.com',
+          'gebruikersnaam' => 'JDijkstra',
+          'wachtwoord_hash' => '$2y$12$qEiiSq6304LVLoa0wp3hw.jMe575sZ9QrePfFOObrpgV0DjnGgG9q',
+          'created_at' => '2026-05-13',
+          'geboortedatum' => '1980-01-30',
+          // 'type' => 'beheer',
+          'voornaam' => 'Jeroen',
+          'achternaam' => 'Dijkstra',
+          'telefoon' => '+31610000015',
+          'deleted_at' => null,
+          'account_id' => 15,
+        ]);
 
-    $mockPdo = $this->createMock(PDO::class);
-    $mockPdo->method('prepare')->willReturn($stmt);
+        $mockPdo = $this->createMock(PDO::class);
+        $mockPdo->method('prepare')->willReturn($stmt);
 
-    $beheerdao = new BeheerDAO($mockPdo);
+        $beheerdao = new BeheerDAO($mockPdo);
 
-    $beheer = $beheerdao->getByUsername('JDijkstra');
+        $beheer = $beheerdao->getByUsername('JDijkstra');
 
-    $this->assertInstanceOf(Beheer::class, $beheer);
-  }
+        $this->assertInstanceOf(Beheer::class, $beheer);
+    }
 }
