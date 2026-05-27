bca04e08967a39854a41f55648eaa0ed1b167d86
Lonneke <lonnekevanoers@gmail.com>
2026-05-27 18:59:11 +0200

    Feat(Helper): Add helper function

---
+++ app/Core/Helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+function __($key): string
+{
+  global $text;
+
+  // Hier worden de key opgesplitst via de punten, zodat
+  // webshop.add vertaal naar ['webshop']['add']
+  $keys = explode('.', $key);
+  $value = $text;
+
+  foreach ($keys as $k) {
+    // Kijk of de value K terugkomst in onze lijst
+    if (!isset($value[$k])) {
+      // Zo nee, return de key.
+      return $key;
+    }
+    $value = $value[$k];
+  }
+
+  return $value;
+}
